474ae478e9127b5f8a6cc36070c2f95f4e8ff6ee
Pawan <Pawanacharaya070@gmail.com>
2019-05-11 14:07:32 +0545

    Modified lab 1 of NM

--- NM Lab/test.cpp
+++ NM Lab/test.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 #include<math.h>
+#include<iomanip>
 #include<conio.h>
 using namespace std;
-#define f(x) (x*x*x*x-4*x-10)
+#define f(x) (x*x-3*x+2)
 #define err 0.001
 int main(){
 	float x1,x2,xm;
@@ -11,18 +12,24 @@ int main(){
 		cout<<"Enter two initial guess"<<endl;
 		cin>>x1>>x2;	
 	}while(f(x1)*f(x2)>0);
-		cout<<"X1"<<ends<<ends<<ends<<ends<<"x2"<<ends<<ends<<ends<<ends<<"f(x1)"<<ends<<ends<<ends<<ends<<"f(x2)"<<ends<<ends<<ends<<ends<<"Xm"<<ends<<ends<<ends<<ends<<"f(Xm)"<<endl;
+	cout<<"X1"<<ends<<"x2"<<ends<<"f(x1)"<<ends<<"f(x2)"<<ends<<"Xm"<<ends<<"f(Xm)"<<endl;
 	do{
 	xm=((x1+x2)/2);
 	if((f(xm)*f(x1))<0){
+		cout.precision(3);
+		cout<<x1<<ends<<x2<<ends<<f(x1)<<ends<<f(x2)<<ends<<xm<<ends<<f(xm)<<endl;
 		x2=xm;
 	}else{
+		cout.precision(3);
+		cout<<x1<<ends<<x2<<ends<<f(x1)<<ends<<f(x2)<<ends<<xm<<ends<<f(xm)<<endl;
 		x1=xm;
+		
 	}
-	cout<<x1<<ends<<ends<<ends<<ends<<x2<<ends<<ends<<ends<<ends<<f(x1)<<ends<<ends<<ends<<ends<<f(x2)<<ends<<ends<<ends<<ends<<xm<<ends<<ends<<ends<<ends<<f(xm)<<endl;
+	cout.precision(3);
+	cout<<fixed;
 	count++;
 	
-}while(fabs(f(xm))>=err);
+}while(fabs(f(xm))>err);
 cout<<"Root is:"<<xm<<endl;
 cout<<"Total iteration is"<<endl<<count;
 getch();
